d05dae3ed62ed96a273e3b45a1906c026892fdd2
= <=>
2025-09-12 13:11:45 -0400

    cli: make temporary CLI executable, illustrate symlink

--- tieto.ts
+++ tieto.ts
@@ -1,8 +1,21 @@
+#!/usr/bin/env -S deno run -A
+
+/**
+ * VERY limited "cli" implementation done mostly to illustrate the class.
+ * This will be replaced with something that uses one of Deno's heavily-tested
+ * CLI libraries.
+ * 
+ * It works for now :) 
+ */
+
 import { Tieto } from "./src/tieto.class.ts";
 
 if (import.meta.main) {
   const [cmd, ...argv] = Deno.args.filter((a) => a !== "--debug");
-  const tieto = new Tieto(); // Uses env vars and defaults
+  // Tieto can work with just env vars and defaults for most cases, 
+  // but debug is passed to be illustrative of functionality.
+  // Turn it off if too noisy for "basic CLI"
+  const tieto = new Tieto({debug: true});
 
   if (cmd === "ingest") {
     const file = argv[0];
